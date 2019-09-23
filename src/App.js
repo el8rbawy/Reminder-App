7abb74655d220c2908f94ea5db6787bcd3c21db4
ahmed <ahmed.essam1799@gmail.com>
2019-09-23 21:30:46 +0200

    Install redux & bootstrap

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
    render() {
       return (
-         <div>
-            Hello World
+         <div className="container">
+            Hello
          </div>
       )
    }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+   reducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+<Provider store={ store }>
+   <App />
+</Provider>
+, document.getElementById('root'));
